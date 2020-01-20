657f347ddea71510e5210e3ff96793095a863794
Pantelis Papageorgiou <pan_papageorgiou@hotmail.com>
2020-01-20 09:34:42 +0200

    MultibranchLeNet architecture added

--- models/models.py
+++ models/models.py
@@ -1,36 +1,34 @@
 import torch.nn as nn
 from torch.autograd import Function
 
-class DANN(nn.Module):
+class MultibranchLeNet(nn.Module):
 
     def __init__(self):
-        # Construct nn.Module superclass from the derived classs DANNet
-        super(DANN, self).__init__()
-        # Construct DANNet architecture
-        self.feature_extractor = nn.Sequential()
-        self.feature_extractor.add_module('f_conv1', nn.Conv2d(3, 64, kernel_size=5))
-        self.feature_extractor.add_module('f_bn1', nn.BatchNorm2d(64))
-        self.feature_extractor.add_module('f_pool1', nn.MaxPool2d(2))
-        self.feature_extractor.add_module('f_relu1', nn.ReLU(True))
-        self.feature_extractor.add_module('f_conv2', nn.Conv2d(64, 50, kernel_size=5))
-        self.feature_extractor.add_module('f_bn2', nn.BatchNorm2d(50))
-        self.feature_extractor.add_module('f_drop1', nn.Dropout2d())
-        self.feature_extractor.add_module('f_pool2', nn.MaxPool2d(2))
-        self.feature_extractor.add_module('f_relu2', nn.ReLU(True))
+        # Construct nn.Module superclass from the derived classs MultibranchLeNet
+        super(MultibranchLeNet, self).__init__()
+        # Construct MultibranchLeNet architecture
+        self.conv1 = nn.Sequential()
+        self.conv1.add_module('c1_conv', nn.Conv2d(3, 32, kernel_size=5))
+        self.conv1.add_module('c1_relu', nn.ReLU(True))
+        self.conv1.add_module('c1_pool', nn.MaxPool2d(2))
 
-        self.label_predictor = nn.Sequential()
-        self.label_predictor.add_module('c_fc1', nn.Linear(50 * 4 * 4, 100))
-        self.label_predictor.add_module('c_bn1', nn.BatchNorm1d(100))
-        self.label_predictor.add_module('c_relu1', nn.ReLU(True))
-        self.label_predictor.add_module('c_drop1', nn.Dropout2d())
-        self.label_predictor.add_module('c_fc2', nn.Linear(100, 100))
-        self.label_predictor.add_module('c_bn2', nn.BatchNorm1d(100))
-        self.label_predictor.add_module('c_relu2', nn.ReLU(True))
-        self.label_predictor.add_module('c_fc3', nn.Linear(100, 10))
-        self.label_predictor.add_module('c_softmax', nn.LogSoftmax(dim=1))
+        self.conv2 = nn.Sequential()
+        self.conv2.add_module('c2_conv', nn.Conv2d(32, 48, kernel_size=5))
+        self.conv2.add_module('c2_relu', nn.ReLU(True))
+        self.conv2.add_module('c2_pool', nn.MaxPool2d(2))
+
+        self.feature_classifier = nn.Sequential()
+        self.feature_classifier.add_module('f_fc1', nn.Linear(48 * 4 * 4, 100))
+        self.feature_classifier.add_module('f_bn1', nn.BatchNorm1d(100))
+        self.feature_classifier.add_module('f_relu1', nn.ReLU(True))
+        self.feature_classifier.add_module('f_fc2', nn.Linear(100, 100))
+        self.feature_classifier.add_module('f_bn1', nn.BatchNorm1d(100))
+        self.feature_classifier.add_module('f_relu2', nn.ReLU(True))
+        self.feature_classifier.add_module('f_fc3', nn.Linear(100, 10))
+        self.feature_classifier.add_module('f_softmax', nn.LogSoftmax(dim=1))
 
         self.domain_classifier = nn.Sequential()
-        self.domain_classifier.add_module('d_fc1', nn.Linear(50 * 4 * 4, 100))
+        self.domain_classifier.add_module('d_fc1', nn.Linear(48 * 4 * 4, 100))
         self.domain_classifier.add_module('d_bn1', nn.BatchNorm1d(100))
         self.domain_classifier.add_module('d_relu1', nn.ReLU(True))
         self.domain_classifier.add_module('d_fc2', nn.Linear(100, 2))
@@ -38,11 +36,14 @@ class DANN(nn.Module):
 
     def forward(self, input, lamda):
         input = input.expand(input.data.shape[0], 3, 28, 28)
-        feature = self.feature_extractor(input)
-        feature = feature.view(-1, 50 * 4 * 4)
-        reverse_feature = ReverseLayer.apply(feature, lamda)
-        class_prediction = self.label_predictor(feature)
+        out1 = self.conv1(input)
+        out2 = self.conv2(out1)
+        print(out2.shape)
+        out_test = out2.view(-1, 48 * 4 * 4)
+        class_prediction = self.feature_classifier(out_test)
+        reverse_feature = ReverseLayer.apply(out_test, lamda)
         domain_prediction = self.domain_classifier(reverse_feature)
+
         return class_prediction, domain_prediction
 
 
